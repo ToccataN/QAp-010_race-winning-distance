389705bf73910532e80325de676e79c9597c2450
ToccataN <charlestonshadowsx@gmail.com>
2018-06-19 15:51:35 -0400

    change name

--- qgen.py
+++ qgen.py
@@ -20,7 +20,7 @@ def get_question():
     #echoback = mainScript.raceProblem(numberOfracers)
     #returnString = json.dumps(echoback)
     returnString = "Hi there"
-    print returnString
+    print(returnString)
     return returnString
 
 if __name__ == '__main__':
