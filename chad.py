cebb10eafc91900d80f5cc68e4e7539553c2c112
shakleton <john.richter@skillsx.com>
2018-06-21 14:19:24 -0400

    Further instructionText update

--- chad.py
+++ chad.py
@@ -1,6 +1,6 @@
 def buildEchoback(assumptionText, questionText, answerVal):
 
-    instructionText = "Assume that each person a) runs at a constant speed in all races, no matter the distance, and b) maintains this same speed start to finish in each race.  Round your answer to the nearest meter -- you're probably going to want to use a calculator."
+    instructionText = "However fast each person may be, assume he/she runs at the same speed in all races, no matter the distance, and that his/her speed is constant from start to finish.  Round your answer to the nearest meter -- you're probably going to want to use a calculator."
 
     echoback = {
         'information' : assumptionText,
