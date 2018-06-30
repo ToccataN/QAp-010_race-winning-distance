3b73b20cdbed953d8fc65b55e12ea535fa4ba011
shakleton <john.richter@skillsx.com>
2018-06-30 14:11:21 -0400

    Change informationText in t001 to a list variable (with single entry).  Odd "T" behavior showing because of this faulty structure perhaps.

--- t001_mainScript.py
+++ t001_mainScript.py
@@ -2,7 +2,7 @@ import chad
 
 def enterNumber(number):
 
-    informationText = "This is a simple test API for set-up."
+    informationText = ["This is a simple test API for set-up."]
     questionText = "Type " + str(number) + " into the box below."
     instructionText = "This is not a trick question"
 
