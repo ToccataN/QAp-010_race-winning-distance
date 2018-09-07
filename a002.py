7499fd97be46b9f02290e321f295d2c97eb69c09
shakleton <john.richter@skillsx.com>
2018-09-07 17:37:49 -0400

    Change wording of a002 instruction.

--- a002.py
+++ a002.py
@@ -37,7 +37,7 @@ def fraction():
     informationText.append(info)
 
     questionText = "What is the fraction?"
-    instructionText = "Enter your answer in format separated by / character, i.e. numer/denom.  Leave values as you first calculated them; don't simplify the fraction, e.g. enter 30/80."
+    instructionText = "Enter your answer as a fraction, i.e. two numbers separated by the / character.  Leave values as you first calculated them; don't simplify the fraction, e.g. enter 30/80."
 
     echoBack = chad.buildEchoback(informationText, questionText, answer, instructionText)
 
